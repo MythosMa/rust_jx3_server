85b6fa8e1184dbdbba7f232a376dff5f84a2b251
unknown <mythosmayuan@gmail.com>
2025-12-12 17:30:21 +0800

    add calendar api

---
+++ .docker/deploy.sh
@@ -0,0 +1,14 @@
+export BUILD_IMG_TAG=0.0.0001
+
+sh .docker/check-docker.sh
+
+cp .docker/Dockerfile ./
+
+docker buildx build \
+   --platform=linux/amd64 \
+   --tag ccr.ccs.tencentyun.com/mythosma/rust-jx3-server:$BUILD_IMG_TAG \
+   --push -t ccr.ccs.tencentyun.com/mythosma/rust-jx3-server:$BUILD_IMG_TAG .
+
+rm -f Dockerfile
+rm -rf dist
+
